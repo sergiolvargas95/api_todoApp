80e8c7e0e6c262c359a55b3e4778236ea2f0f248
sergiolvargas95 <sergiolvargas95@gmail.com>
2025-02-21 06:22:36 -0500

    add: foreign keys users, todos and categories

---
+++ dba/004_add_foreign_keys_users_todos_categories.sql
@@ -0,0 +1,9 @@
+-- add foreign key from users to todos
+ALTER TABLE todos
+ADD COLUMN user_id INT NOT NULL,
+ADD CONSTRAINT fk_todos_users FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+
+-- add foreign key from todos to categories
+ALTER TABLE todos
+ADD COLUMN category_id INT NOT NULL,
+ADD CONSTRAINT fk_todos_categories FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE RESTRICT;
--- dba/README.md
+++ dba/README.md
@@ -9,3 +9,4 @@
 - **001_create_users_table.sql**: Creates `users` table.
 - **002_create_categories_table.sql**: Creates `categories` table.
 - **003_create_todos_table.sql**: Creates `todos` table.
+- **004_add_foreign_keys_users_todos_categories**: Alter `todos` table.
